35e9b22f74e725ec39f03a822501159a8ed116d0
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-03 01:38:19 +0700

    idea for test

---
+++ test/hooks/useProvider.test.tsx
@@ -0,0 +1,6 @@
+// able to get data: class, primitive
+// able to get data created by Create without update
+// able to get data created by Value with update
+// able to get data from deeply nested provider
+// query by specified name
+// error test

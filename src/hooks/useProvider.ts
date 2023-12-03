66fcc3de58c71f8514e91cdb84e53ca8b0637c1d
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-03 22:33:52 +0700

    make useProvider use useNamedProvider

--- src/hooks/useProvider.ts
+++ src/hooks/useProvider.ts
@@ -1,7 +1,5 @@
-import { useContext } from 'react'
 import { getObjectRuntimeName } from '../utils'
-import { ProviderContext } from '../ProviderContext'
-import { ResourcesNotProvidedError } from '../errors'
+import useNamedProvider from './useNamedProvider'
 
 function useProvider(ctor: StringConstructor, name?: string): string
 function useProvider(ctor: BooleanConstructor, name?: string): boolean
@@ -9,14 +7,7 @@ function useProvider(ctor: NumberConstructor, name?: string): number
 function useProvider<T>(ctor: new (...a: any) => T, name?: string): T
 function useProvider<T>(ctor: new (...a: any) => T, name?: string): T {
   const key = name ?? getObjectRuntimeName(ctor)
-  const parentDataContext = useContext(ProviderContext)
-  const parentData = parentDataContext?.data
-  const parentDataExisted = parentData?.has(key) ?? false
-  if (!parentDataExisted) {
-    throw new ResourcesNotProvidedError(key)
-  }
-  const result = parentData?.get(key)
-  return result
+  return useNamedProvider(key)
 }
 
 export default useProvider

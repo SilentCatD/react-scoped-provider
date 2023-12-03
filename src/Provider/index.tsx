9f0fefc279e396cb5ba2a4d19a252702c5cee7f3
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-03 21:11:38 +0700

    add new functionality cleanup in provider, add test for it

--- src/Provider/index.tsx
+++ src/Provider/index.tsx
@@ -1,15 +1,24 @@
-import { PropsWithChildren, useRef } from 'react'
+import { PropsWithChildren, useEffect, useRef } from 'react'
 import { Create } from '../types'
 import { isCreate } from '../utils'
 import { ProviderScope } from '../ProviderScope'
 
 type ProviderProps<T> = {
   source: Create<T> | T
+  cleanUp?: (data: T) => void
   name?: string
 }
-function Provider<T>({ source, children, name }: PropsWithChildren<ProviderProps<T>>): JSX.Element {
+function Provider<T>({ source, cleanUp, children, name }: PropsWithChildren<ProviderProps<T>>): JSX.Element {
   const createdData = useRef(isCreate(source) ? source() : undefined)
   const valueData: T | undefined = isCreate(source) ? undefined : source
+  useEffect(() => {
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      const currentData = createdData.current
+      cleanUp?.(currentData ?? valueData!)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <ProviderScope name={name} value={createdData.current ?? valueData}>
       {children}
---
+++ test/Provider/cleanup.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react'
+import { Provider } from '../../src'
+
+it('cleanup provided value', () => {
+  const cleanUpFunc = jest.fn((value) => value)
+  const { unmount } = render(
+    <Provider source={'value'} cleanUp={cleanUpFunc}>
+      <div></div>
+    </Provider>,
+  )
+  unmount()
+  expect(cleanUpFunc.mock.calls).toHaveLength(1)
+  expect(cleanUpFunc.mock.calls[0][0]).toBe('value')
+})
+
+class A {}
+
+it('cleanup provided Create', () => {
+  const instance = new A()
+  const cleanUpFunc = jest.fn((value) => value)
+  const { unmount } = render(
+    <Provider source={() => instance} cleanUp={(value) => cleanUpFunc(value)}>
+      <div></div>
+    </Provider>,
+  )
+  unmount()
+  expect(cleanUpFunc.mock.calls).toHaveLength(1)
+  expect(cleanUpFunc.mock.calls[0][0]).toBe(instance)
+})

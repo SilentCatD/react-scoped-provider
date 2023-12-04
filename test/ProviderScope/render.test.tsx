7525aabcf22baa0fac4cd74028f150580b05edbe
Nguyễn Ngọc Phước <36961907+SilentCatD@users.noreply.github.com>
2023-12-04 07:04:51 +0700

    add more test (#15)

--- test/ProviderScope/render.test.tsx
+++ test/ProviderScope/render.test.tsx
@@ -1,14 +1,27 @@
 import { getByTestId, render } from '@testing-library/react'
 import { ProviderScope } from '../../src'
+import { PropsWithChildren } from 'react'
 
 const DisplayRendered = () => {
   return <h1 data-testid='text'>Rendered</h1>
 }
 
+const Nester = ({ children }: PropsWithChildren) => {
+  return <div>{children}</div>
+}
+
 it('render without error', () => {
   const { container } = render(
     <ProviderScope value={'value'}>
-      <DisplayRendered />
+      <Nester>
+        <Nester>
+          <Nester>
+            <Nester>
+              <DisplayRendered />
+            </Nester>
+          </Nester>
+        </Nester>
+      </Nester>
     </ProviderScope>,
   )
   const rendered = getByTestId(container, 'text')
